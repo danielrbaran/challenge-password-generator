59f8add2f1a70b548461bf453357d42caa8adb02
danielrbaran <53953292+danielrbaran@users.noreply.github.com>
2022-12-22 08:19:23 -0800

    add starter code to try to produce a polished product by tonnight's deadline

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ var userNumChars = prompt("Please input the number of characters you'd like in y
 
 // ask user if they'd like to include each character type successively
 var charUpper = confirm("Would you like to include upper case characters?");
+
+
+
+
+
+
+
+
+
+
+
+// Starter code below
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
